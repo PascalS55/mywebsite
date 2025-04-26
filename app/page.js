244a3a36a5ef6fa0666e3ca4df8894ea61437ca1
PascalS55 <94374772+PascalS55@users.noreply.github.com>
2025-04-26 18:04:27 +0200

    updated CV

--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link
-                  href='/Personal_CV.pdf'
+                  href='/Resume_PS.pdf'
                   target={"_blank"}
                   className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light
                   hover:text-dark border-2 border-solid border-transparent hover:border-dark md:p-2 md:px-4 md:text-base'
